5855c55d2016575da79b6e09a55a9ca36a10ef00
bhalle <bhalle@jrnn21.mobile.asu.edu>
2010-10-04 11:40:42 -0700

    update templates for partner and bundle list

--- news21national/bundle/admin.py
+++ news21national/bundle/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 from news21national.bundle.models import StoryBundle
 
-admin.site.register(StoryBundle)
-
 class StoryBundleAdmin(admin.ModelAdmin):
-	pass
+	def save_model(self, request, obj, form, change): 
+			instance = form.save(commit=False)
+			if not hasattr(instance, 'created_by'):
+				instance.created_by = request.user
+			instance.modified_by = request.user
+			instance.save()
+			form.save_m2m()
+			return instance
+
+	def save_formset(self, request, form, formset, change): 
+		def set_user(instance):
+			if not hasattr(instance, 'created_by'):
+				instance.created_by = request.user
+			instance.modified_by = request.user
+			instance.save()
+
+		if formset.model == User:
+			instances = formset.save(commit=False)
+			map(set_user, instances)
+			formset.save_m2m()
+			return instances
+		else:
+			return formset.save()
+
+
+admin.site.register(StoryBundle)
--- news21national/bundle/views.py
+++ news21national/bundle/views.py
@@ -7,8 +7,11 @@ from django.shortcuts import get_object_or_404
 
 from news21national.bundle.models import StoryBundle
 from news21national.story.models import Story
+from news21national.multimedia.models import Media
 
 def get_bundle(request,slug):
 	bundle = get_object_or_404(StoryBundle, slug=slug)
 	stories = Story.objects.filter(id__in=bundle.stories.values_list('id',flat=True))
-	return render_to_response('bundle/list.html', { 'bundle': bundle, 'stories':stories }, context_instance=RequestContext(request))
+	images = Media.children.filter(id__in=stories.values_list('primary_image',flat=True),_child_name="photo",status="Approved")
+	multimedia = Media.children.filter(story__in=stories.values_list('id',flat=True),status="Approved")
+	return render_to_response('bundle/list.html', { 'bundle': bundle, 'stories':stories, 'images':images,'multimedia':multimedia }, context_instance=RequestContext(request))
