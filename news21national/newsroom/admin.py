b9dd34f15ab9487e098d4a6050a7b6fd892a8ccd
bhalle <bhalle@jrnn21.mobile.asu.edu>
2011-01-25 13:42:50 -0700

    updated newsroom admin form to display detail members info

--- news21national/newsroom/admin.py
+++ news21national/newsroom/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from news21national.newsroom.models import Newsroom, NewsOrganization
-
+from news21national.core.models import Profile
+from django import forms
 
 class NewsOrganizationAdmin(admin.ModelAdmin):
 	list_display = ('name','shorter_code','site_url')
@@ -14,7 +15,20 @@ class NewsOrganizationAdmin(admin.ModelAdmin):
 			return db_field.formfield(**kwargs)
 		return super(NewsOrganizationAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
+
+class NewsroomForm(forms.ModelForm):
+
+	def __init__(self, *args, **kwargs):
+		super(NewsroomForm, self).__init__(*args, **kwargs)
+		profiles = Profile.objects.all();
+		s = self.fields['members'].widget
+		choices = []
+		for choice in profiles:
+			choices.append((choice.id, '('+choice.school+') '+choice.first_name+ ' '+choice.last_name+' ( '+str(choice.user.email)+' )' ))
+		s.choices = sorted(choices, key=lambda c: c[1])
+
 class NewsroomAdmin(admin.ModelAdmin):
+	form = NewsroomForm
 	list_display = ('name','site_url','shorter_code','organization','is_active')
 
 	def formfield_for_foreignkey(self, db_field, request, **kwargs):
