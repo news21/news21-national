a38487fd61e10855d2465feb13264fc7a3d7efeb
bhalle <bhalle@jrnn21.mobile.asu.edu>
2011-02-15 11:54:51 -0700

    update bundle filter

--- news21national/partner/admin.py
+++ news21national/partner/admin.py
@@ -26,7 +26,7 @@ class StoryPlacementsForm(forms.ModelForm):
 class StoryPlacementsAdmin(admin.ModelAdmin):
 	form = StoryPlacementsForm
 	list_display = ('story','partner','placement_headline')
-	search_fields = ['story','partner','description','placement_headline']
+	search_fields = ['story','partner','description','placement_headline',]
 
 admin.site.register(StoryPlacements,StoryPlacementsAdmin)
 
