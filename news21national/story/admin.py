114c856835759c5dd921e98deead494edc995182
bhalle <bhalle@jrnn21.mobile.asu.edu>
2011-02-17 11:50:15 -0700

    added inline form into models that can receive awards

--- news21national/story/admin.py
+++ news21national/story/admin.py
@@ -1,19 +1,28 @@
 from django.contrib import admin
 from news21national.story.models import MetaStory,Story,Project
 
+from news21national.awards.models import Award
+from django.contrib.contenttypes import generic
+
+class AwardInline(generic.GenericTabularInline):
+		model = Award
+
 class ProjectAdmin(admin.ModelAdmin):
 	list_display = ('id','admin_image','admin_screenshot','started_at','name','description')
 	search_fields = ['name','description']
+	inlines = [AwardInline,]
 
 class MetaStoryAdmin(admin.ModelAdmin):
 	list_display = ('id','headline','project','summary','status')
 	list_filter = ['status','newsrooms']
 	search_fields = ['headline','sub_headline','summary','process']
+	inlines = [AwardInline,]
 
-class StoryAdmin(admin.ModelAdmin):	
+class StoryAdmin(admin.ModelAdmin): 
 	list_display = ('id','headline','metastory','status')
 	list_filter = ['status','metastory']
 	search_fields = ['headline','summary','process']
+	inlines = [AwardInline,]
 
 admin.site.register(Project,ProjectAdmin)
 admin.site.register(MetaStory,MetaStoryAdmin)
